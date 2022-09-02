fe7a7ee8ee5d0ff1c2866fea452750a0d12079a0
Satyam Saha <52621047+satyamsaha443@users.noreply.github.com>
2022-09-02 11:38:29 +0530

    Update random.h

--- random.h
+++ random.h
@@ -1,5 +1,3 @@
-
-
 static long MULTIPLIER  = 1366;
 static long ADDEND      = 150889;
 static long PMOD        = 714025;
@@ -11,21 +9,11 @@ double drandom()
     long random_next;
     double ret_val;
 
-// 
-// compute an integer random number from zero to mod
-//
     random_next = (MULTIPLIER  * random_last + ADDEND)% PMOD;
     random_last = random_next;
-
-//
-// shift into preset range
-//
     ret_val = ((double)random_next/(double)PMOD)*(random_hi-random_low)+random_low;
     return ret_val;
 }
-//
-// set the seed and the range
-//
 void seed(double low_in, double hi_in)
 {
    if(low_in < hi_in)
@@ -38,9 +26,6 @@ void seed(double low_in, double hi_in)
       random_low = hi_in;
       random_hi  = low_in;
    }
-   random_last = PMOD/ADDEND;  // just pick something
+   random_last = PMOD/ADDEND;
 
 }
-//**********************************************************
-// end of pseudo random generator code.
-//**********************************************************
