65d8ce6b9871edea97f07abea1edf98828d411eb
Satyam Saha <52621047+satyamsaha443@users.noreply.github.com>
2022-09-08 21:22:56 +0530

    Create prod_cons.c

---
+++ prod_cons.c
@@ -0,0 +1,47 @@
+#include <omp.h>
+#ifdef APPLE
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#endif
+#include <stdio.h>
+
+#define N        10000
+#define SEED       2531
+#define RAND_MULT  1366
+#define RAND_ADD   150889
+#define RAND_MOD   714025
+int randy = SEED;
+void fill_rand(int length, double *a)
+{
+   int i; 
+   for (i=0;i<length;i++) {
+     randy = (RAND_MULT * randy + RAND_ADD) % RAND_MOD;
+     *(a+i) = ((double) randy)/((double) RAND_MOD);
+   }   
+}
+double Sum_array(int length, double *a)
+{
+   int i;  double sum = 0.0;
+   for (i=0;i<length;i++)  sum += *(a+i);  
+   return sum; 
+}
+  
+int main()
+{
+  double *A, sum, runtime;
+  int flag = 0;
+
+  A = (double *)malloc(N*sizeof(double));
+
+  runtime = omp_get_wtime();
+
+  fill_rand(N, A);      
+
+  sum = Sum_array(N, A);  
+   
+  runtime = omp_get_wtime() - runtime;
+
+  printf(" In %f seconds, The sum is %f \n",runtime,sum);
+}
+ 
